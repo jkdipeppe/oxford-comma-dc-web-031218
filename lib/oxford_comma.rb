8352a36581bb0dcfe3a946b70f252f15653ce14c
Joseph DiPeppe <jkdipeppe@gmail.com>
2018-03-01 19:11:56 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,12 +4,13 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   else
+    last_string = "and #{array[-1]}"
     arr_final = Array.new
     arr_final = array
     arr_final.pop
     final_string = String.new
     final_string = arr_final.join(", ")
-    last_string = "and #{array[-1]}"
+    
     arr_final.push(last_string)
   end
 
